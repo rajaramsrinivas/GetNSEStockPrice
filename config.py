b3953b686535349e413575c19569a881a1d5483e
Rajaram Srinivasan <rajaram.1990@gmail.com>
2016-05-30 23:35:46 +0530

    Adding config file

---
+++ config.py
@@ -0,0 +1,7 @@
+config = {'smtp_sender' : 'Test User <testuser@gmail.com>',
+          'smtp_receivers': ['test@gmail.com'],
+          'smtp_server' : 'smtp.gmail.com',
+          'smtp_port' : 465,
+          'smtp_login' : 'test@gmail.com',
+          'smtp_password': 'testpassword'
+         }
