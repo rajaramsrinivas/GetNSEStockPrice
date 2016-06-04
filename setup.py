2ef69e983dbfdda9008b65bbf1faa7fe458df1a4
Rajaram Srinivasan <rajaram.1990@gmail.com>
2016-06-05 00:45:38 +0530

    Fixing syntax issues with setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
   name = 'GetNSEStockPrice',
-  packages = ['GetNSEStockPrice'], # this must be the same as the name above
+  packages = ['GetNSEStockPrice'],
   version = '0.1',
   description = 'Simple python library that consumes HTTP REST data of sttock prices provided by Google and Yahoo Finance APIs.',
   author = 'Rajaram Srinivasan',
   author_email = 'rajaram.1990@gmail.com',
-  url = 'https://github.com/rajaram1990/GetNSEStockPrice', # use the URL to the github repo
+  url = 'https://github.com/rajaram1990/GetNSEStockPrice',
   download_url = 'https://github.com/rajaram1990/GetNSEStockPrice/tarball/0.1'
-  keywords = ['Finance', 'Stock', 'NSE', 'Google', 'Yahoo', 'LTP'], # arbitrary keywords
+  keywords = ['Finance', 'Stock', 'NSE', 'Google', 'Yahoo', 'LTP'],
   classifiers = [],
 )
