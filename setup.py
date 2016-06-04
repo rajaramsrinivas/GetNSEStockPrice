358ec7aea4ed95260faecfd5210c76dd7abb6aee
Rajaram Srinivasan <rajaram.1990@gmail.com>
2016-06-05 00:30:08 +0530

    Adding setup, license for including in pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'GetNSEStockPrice',
+  packages = ['GetNSEStockPrice'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Simple python library that consumes HTTP REST data of sttock prices provided by Google and Yahoo Finance APIs.',
+  author = 'Rajaram Srinivasan',
+  author_email = 'rajaram.1990@gmail.com',
+  url = 'https://github.com/rajaram1990/GetNSEStockPrice', # use the URL to the github repo
+  download_url = 'https://github.com/rajaram1990/GetNSEStockPrice/tarball/0.1'
+  keywords = ['Finance', 'Stock', 'NSE', 'Google', 'Yahoo', 'LTP'], # arbitrary keywords
+  classifiers = [],
+)
